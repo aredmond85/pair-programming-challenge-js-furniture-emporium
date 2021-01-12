60b8cd8b038e2ba13aca213b6bab9a45e12cabdf
aredmond85 <aredmond85@gmail.com>
2021-01-11 20:23:51 -0600

    first commit

--- app/index.js
+++ app/index.js
@@ -1,5 +1,27 @@
 // step one: fetch all furniture
 
+function parseJSON(response) {
+    return response.json()
+}
+
+function fetchFurniture() {
+    fetch(`http://localhost:3000/furniture`)
+        .then(parseJSON)
+        .then(json => renderItems(json))
+}
+
 // step two: add all furniture to the ul
+function renderItems(items) {
+    const ul = document.querySelector('ul')
+    items.forEach(element => {
+        const li = document.createElement('li')
+        li.innerText = `${element.name} ${element.price}` 
+        ul.appendChild(li) 
+    })
+}
 
 // step three: ???
+
+//Execute functions
+fetchFurniture()
+
