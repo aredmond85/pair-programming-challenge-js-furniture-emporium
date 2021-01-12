fd5c6a2941e1ab27887f0a925ef3f5f17c6ca22f
aredmond85 <aredmond85@gmail.com>
2021-01-11 21:13:26 -0600

    fetch works, working on filling form

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 // step one: fetch all furniture
+const URL = "http://localhost:3000"
+const nameInput = document.querySelectorAll("input")[0]
+const descInput = document.querySelectorAll("input")[1]
+const priceInput = document.querySelectorAll("input")[2]
+const editForm = document.querySelector("#furniture-edit-form")
 
 function parseJSON(response) {
     return response.json()
 }
 
 function fetchFurniture() {
-    fetch(`http://localhost:3000/furniture`)
+    fetch(URL + `/furniture`)
         .then(parseJSON)
         .then(json => renderItems(json))
 }
@@ -15,13 +20,16 @@ function renderItems(items) {
     const ul = document.querySelector('ul')
     items.forEach(element => {
         const li = document.createElement('li')
-        li.innerText = `${element.name} ${element.price}` 
-        ul.appendChild(li) 
+        li.innerText = `${element.name} ${element.price}`
+        ul.appendChild(li)
     })
+}
+
+function fillForm(data){
+
 }
 
 // step three: ???
 
 //Execute functions
 fetchFurniture()
-
