dd09079f4fdb43866266e75709cafdb76f619904
aredmond85 <aredmond85@gmail.com>
2021-01-13 20:45:27 -0600

    done, everything working

--- app/index.js
+++ app/index.js
@@ -1,7 +1,5 @@
 const URL = "http://localhost:3000"
 
-const editForm = document.querySelector("#furniture-edit-form")
-
 function parseJSON(response) {
     return response.json()
 }
@@ -10,31 +8,6 @@ function fetchFurniture() {
     fetch(URL + `/furniture`)
         .then(parseJSON)
         .then(json => renderItems(json))
-}
-
-function renderItems(items) {
-    const ul = document.querySelector('ul')
-    items.forEach(element => {
-        const li = document.createElement('li')
-        const p = document.createElement('p')
-        li.innerText = `${element.name} $${element.price}`
-        p.innerText = `${element.description}`
-        p.style.display = "none"
-        li.dataset.furnitureId = element.id
-        ul.appendChild(li)
-        li.addEventListener("click", fillForm)
-    })
-}
-
-function fillForm(event) {
-    changeColor(event.target)
-    const nameInput = document.getElementById("furniture-name")
-   const descInput = document.getElementById("furniture-description")
-    const priceInput = document.getElementById("furniture-price")
-    const [itemName, price] = event.target.innerText.split(" $")
-    nameInput.value = itemName
-    priceInput.value = price
-    console.log(descInput.value)
 }
 
 function changeColor(li) {
@@ -51,6 +24,50 @@ function clearColor() {
 
 function getLis() {
     return document.querySelectorAll("li")
+}
+
+function renderItems(items) {
+    const ul = document.querySelector('ul')
+    items.forEach(element => {
+        const li = document.createElement('li')
+        const p = document.createElement('p')
+        li.innerText = `${element.name} $${element.price}`
+        p.innerText = `${element.description}`
+        p.style.display = "none"
+        li.dataset.furnitureId = element.id
+        ul.appendChild(li)
+        li.appendChild(p)
+        li.addEventListener("click", fillForm)
+    })
+}
+
+function fillForm(event) {
+    changeColor(event.target)
+    const nameInput = document.getElementById("furniture-name")
+    const descInput = document.getElementById("furniture-description")
+    const priceInput = document.getElementById("furniture-price")
+    const [itemName, price] = event.target.innerText.split(" $")
+    const description = event.target.childNodes[1].innerText
+    const liId = event.target.dataset.furnitureId
+
+    nameInput.value = itemName
+    priceInput.value = price
+    descInput.value = description
+
+    editForm(liId)
+}
+
+function editForm(id) {
+    const selectedLi = document.querySelector(`[data-furniture-id='${id}']`)
+    const changeForm = document.getElementById("furniture-edit-form")
+    const nameInput = document.getElementById("furniture-name")
+    const descInput = document.getElementById("furniture-description")
+    const priceInput = document.getElementById("furniture-price")
+
+    changeForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+        selectedLi.innerHTML = `${nameInput.value} $${priceInput.value} <p style="display: none;">${descInput.value}</p>`
+    })
 }
 
 //Execute functions
